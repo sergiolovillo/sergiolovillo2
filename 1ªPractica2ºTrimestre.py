decd21687660a0e253316d87ef988fde0d1bbdff
sergiolovillo <sergiolovillo@outlook.com>
2017-02-19 11:40:43 +0100

    Add files via upload

---
+++ 1ªPractica2ºTrimestre.py
@@ -0,0 +1,23 @@
+class Alumno:
+    'Clase base común para todos los alumnos'
+    totalAlum = 0
+
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        Alumno.totalAlum += 1
+
+    def totalAlumnos(self):
+        print ("Total de alumnos %d" % Alumno.totalAlum)
+
+    def caracAlumnos(self):
+        print ("Nombre: ", self.nombre, ", Edad: ", self.edad)
+
+
+"Esto creará el primer objeto de la clase Alumno."
+alum1 = Alumno("Antonio", 25)
+"Esto creará el segundo objeto de la clase Alumno."
+alum2 = Alumno("Ana", 21)
+alum1.caracAlumnos()
+alum2.caracAlumnos()
+print ("Total de alumnos %d" % Alumno.totalAlum)
